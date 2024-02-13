78965703c77a782ab815d923c6b78967dfece014
nechamdod <nechamdod@gmail.com>
2024-02-13 21:49:02 +0200

    init: chat with VA

--- controllers/virtual_assistant/__init__.py
+++ controllers/virtual_assistant/__init__.py
@@ -1,6 +1,6 @@
 from aiogram import Router
-
+from . import chat
 def prepare_router() -> Router:
     virtual_assistant = Router()
-
+    virtual_assistant.callback_query.register(chat.callback_start_chat, lambda c: c.data == "/chat_with_va")
     return virtual_assistant
